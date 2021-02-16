8ce1cad0edb6a24b35bd72a38650317fdc698d17
James Montemagno <james.montemagno@gmail.com>
2021-02-16 11:57:32 -0800

    Create initenvironment.sh

---
+++ initenvironment.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Hi!
+# If you're reading this, you're probably interested in what's 
+# going on within this script. We've provided what we hope are useful
+# comments inline, as well as color-coded relevant shell output.
+# We hope it's useful for you, but if you have any questions or suggestions
+# please open an issue on https://github.com/MicrosoftDocs/mslearn-xamarin-consume-rest-services.
+#
+
+## Start
+
+declare moduleName=consume-rest-services
+
+# GitHub
+echo "declaring variables"
+gitrepo=https://github.com/MicrosoftDocs/mslearn-xamarin-consume-rest-services
+gitpath=src/webservice/BookServer/BookServer.csproj
+
+# dotnet SDK version
+declare -x dotnetSdkVersion="3.1.406"
+
+# Figure out the name of the resource group to use
+declare resourceGroupName=""
+declare existingResourceGroup=$(az group list | jq '.[] | select(.tags."x-created-by"=="freelearning").name' --raw-output)
+
+# If there is more than one RG or there's only one but its name is not a GUID,
+# we're probably not in the Learn sandbox.
+if ! [ $existingResourceGroup ]
+then
+    echo "${warningStyle}WARNING!!!" \
+        "It appears you aren't currently running in a Microsoft Learn sandbox." \
+        "Any Azure resources provisioned by this script will result in charges" \
+        "to your Azure subscription.${defaultTextStyle}"
+    resourceGroupName=$moduleName
+else
+    resourceGroupName=$existingResourceGroup
+fi
+
+echo "Using Azure resource group $resourceGroupName."
+
+# Azure
+webappname=mslearnbookserver$RANDOM$RANDOM
+echo "Creating app service plan"
+az appservice plan create --name $webappname --resource-group $resourceGroupName --sku FREE
+
+echo "creating web app"
+az webapp create --name $webappname --resource-group $resourceGroupName --plan $webappname
+
+echo "Configuring web app"
+az webapp config appsettings set -g $resourceGroupName -n $webappname --settings PROJECT=$gitpath
+
+echo "Deploying web app"
+az webapp deployment source config --name $webappname --resource-group $resourceGroupName --repo-url $gitrepo --branch master --manual-integration
+
+echo "Web app deployed! Here is the url to use in the app:"
+echo https://$webappname.azurewebsites.net
